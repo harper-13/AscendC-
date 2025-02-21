74fa61d963cb826ac19579d2d2cd5a8d7da36b89
harper-13 <harpernju@gmail.com>
2025-02-21 21:25:35 +0800

    Add files via upload

---
+++ swish/AclNNInvocation/build/CMakeFiles/execute_swish_op.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/root/scj/swish/AclNNInvocation/output/execute_swish_op"
+  "/root/scj/swish/AclNNInvocation/output/execute_swish_op.pdb"
+  "CMakeFiles/execute_swish_op.dir/main.cpp.o"
+  "CMakeFiles/execute_swish_op.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/execute_swish_op.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ swish/AclNNInvocation/scripts/gen_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Copyright 2022-2023 Huawei Technologies Co., Ltd
+import numpy as np
+import os
+
+def gen_golden_data_simple():
+    input_x = np.random.uniform(-100, 100, [8, 200, 1024]).astype(np.float32)
+    golden = input_x / (1+ np.exp(-input_x))
+    os.system("mkdir -p input")
+    os.system("mkdir -p output")
+    input_x.tofile("./input/input_x.bin")
+    golden.tofile("./output/golden.bin")
+
+if __name__ == "__main__":
+    gen_golden_data_simple()
